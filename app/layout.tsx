730238d7f49a08abdaaa484e9c885b7d80fa1566
danmend43 <danmend43@gmail.com>
2025-05-29 02:27:26 -0300

    ty6

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 
 export default function RootLayout({
+
   children,
 }: {
   children: React.ReactNode
@@ -19,5 +20,6 @@ export default function RootLayout({
 }
 
 export const metadata = {
-      generator: 'v0.dev'
+      title: 'Perfil',
+      
     };
